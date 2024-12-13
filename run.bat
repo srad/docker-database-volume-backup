9c54a9b43210382215e7c9c210b0a92903540ab0
srad <ssedighi@posteo.de>
2024-12-13 16:09:56 +0100

    Ignore local custom docker-compose.

--- run.bat
+++ run.bat
@@ -4,5 +4,5 @@ docker image prune -ya
 docker build -t docker-database-volume-backup:dev .
 if %errorlevel% neq 0 exit /b %errorlevel%
 docker stack rm backup
-docker stack deploy -c docker-compose.yml backup
+docker stack deploy -c docker-compose.local.yml backup
 docker service logs backup_docker-database-volume-backup -f
